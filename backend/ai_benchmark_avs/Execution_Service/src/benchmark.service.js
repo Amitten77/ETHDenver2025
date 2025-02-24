17397b417cd542acc02bcbd5677cca7874317204
Dylan Subramanian <dylansubramanian@gmail.com>
2025-02-24 16:31:21 -0700

    added setup + ids

--- backend/ai_benchmark_avs/Execution_Service/src/benchmark.service.js
+++ backend/ai_benchmark_avs/Execution_Service/src/benchmark.service.js
@@ -1,23 +1,24 @@
-axios = require('axios');
-
+axios = require("axios");
 
 async function getAccuracy(query) {
   try {
-    const response = await axios.get(`http://host.docker.internal:5179/get_benchmark`, {
-      params: { query }
-    });
-    
-    return {
-      accuracy: response.data.accuracy
-    };
+    const response = await axios.get(
+      `http://host.docker.internal:5179/get_benchmark`,
+      {
+        params: { query },
+      }
+    );
 
+    return {
+      accuracy: response.data.accuracy,
+      id: Math.random(),
+    };
   } catch (error) {
     console.error("Error fetching benchmark data:", error);
     return { error: "Failed to retrieve accuracy" };
   }
 }
 
-  
 module.exports = {
   getAccuracy,
-}
+};
