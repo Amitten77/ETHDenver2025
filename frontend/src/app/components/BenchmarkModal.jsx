3922f86923a33cdf0970ccc4678c1c15588187da
Dylan Subramanian <dylansubramanian@gmail.com>
2025-02-27 14:20:02 -0700

    Added frontend fixes

--- frontend/src/app/components/BenchmarkModal.jsx
+++ frontend/src/app/components/BenchmarkModal.jsx
@@ -3,9 +3,7 @@ import { useState } from "react";
 import { ethers } from "ethers";
 
 const BenchmarkModal = ({ benchmark, onClose }) => {
-
-  const [diffBenchmark, setDiffBenchmark] = useState('');
-
+  const [diffBenchmark, setDiffBenchmark] = useState("");
 
   async function fetchTransaction(transactionHash) {
     const provider = new ethers.JsonRpcProvider(
@@ -16,15 +14,16 @@ const BenchmarkModal = ({ benchmark, onClose }) => {
 
     const hexString = inputData.data;
 
-
-    const cleanedHex = hexString.startsWith("0x") ? hexString.slice(2) : hexString;
+    const cleanedHex = hexString.startsWith("0x")
+      ? hexString.slice(2)
+      : hexString;
 
     const decodedText = Buffer.from(cleanedHex, "hex").toString("utf8");
 
     const jsonMatch = decodedText.match(/\{.*\}/s); // Extracts content within {}
 
     if (!jsonMatch) {
-        throw new Error("No valid JSON found");
+      throw new Error("No valid JSON found");
     }
 
     const jsonString = jsonMatch[0];
@@ -32,85 +31,86 @@ const BenchmarkModal = ({ benchmark, onClose }) => {
     return jsonString;
   }
 
-
   const handleAVS = async (avsModel, avsBenchMark) => {
     try {
-      var taskResponse = await fetch('http://localhost:4003/task/execute', {
-        method: 'POST',
+      var taskResponse = await fetch("http://localhost:4003/task/execute", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ model: avsModel, benchmark: avsBenchMark }),
       });
 
       if (!taskResponse.ok) {
-        throw new Error('Failed to execute task');
+        throw new Error("Failed to execute task");
       }
 
       var taskResult = await taskResponse.json();
       alert(`Execution Result: ${JSON.stringify(taskResult)}`);
 
-      const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+      const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
       await sleep(20000);
 
-      const transactionResponse = await fetch('http://localhost:1011/latest-tx');
+      const transactionResponse = await fetch(
+        "http://localhost:1011/latest-tx"
+      );
 
       const transactionData = await transactionResponse.json();
 
       console.log(transactionData);
 
       if (!transactionData.transactionHash) {
-        throw new Error('No transaction found');
+        throw new Error("No transaction found");
       }
       // Fetch the transaction hash
       const transactionHash = transactionData.transactionHash;
-      const inputData = await fetchTransaction(transactionHash)
-      
+      const inputData = await fetchTransaction(transactionHash);
+
+      console.log(inputData, "InputData");
+
       const jsonInputData = JSON.parse(inputData);
 
       console.log(jsonInputData);
 
       var mongoModel = jsonInputData.model;
 
-      console.log(mongoModel)
+      console.log(mongoModel);
 
       if (jsonInputData.model.includes("http")) {
         mongoModel = jsonInputData.model.split("/").pop().split(".")[1];
       }
 
-      var mongoResponse = await fetch('http://localhost:3536/add_model_data', {
-        method: 'POST',
+      var mongoResponse = await fetch("http://localhost:3536/add_model_data", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({ model: mongoModel, benchmark: jsonInputData.benchmark, accuracy: jsonInputData.accuracy, confusion_matrix: jsonInputData.confusion_matrix, transactionHash: transactionHash }),
+        body: JSON.stringify({
+          model: mongoModel,
+          benchmark: jsonInputData.benchmark,
+          accuracy: jsonInputData.accuracy,
+          confusion_matrix: jsonInputData.confusion_matrix,
+          transactionHash: transactionHash,
+        }),
       });
 
       if (!mongoResponse.ok) {
-        console.error('Error:', mongoResponse);
-        throw new Error('Failed to execute task');
+        console.error("Error:", mongoResponse);
+        throw new Error("Failed to execute task");
       }
 
       var mongoResult = await mongoResponse.json();
 
       alert(`Result: ${JSON.stringify(mongoResult)}`);
-
-
     } catch (error) {
-      console.error('Error:', error);
-      alert('Error executing task');
+      console.error("Error:", error);
+      alert("Error executing task");
     }
-
-
-  }
-
-  const handleSubmit = async () => {
-
-    handleAVS(benchmark.model, diffBenchmark);
-
   };
 
-
+  const handleSubmit = async () => {
+    handleAVS(benchmark.model, diffBenchmark);
+  };
 
   return (
     <div className="modal-overlay" onClick={onClose}>
@@ -154,7 +154,10 @@ const BenchmarkModal = ({ benchmark, onClose }) => {
           </select>
         </div>
 
-        <button className="w-64 p-2 bg-blue-500 text-white rounded" onClick={handleSubmit}>
+        <button
+          className="w-64 p-2 bg-blue-500 text-white rounded"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </div>
--- frontend/src/app/components/DelegateInput.jsx
+++ frontend/src/app/components/DelegateInput.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
 
-const DelegateInput = ({operator}) => {
+const DelegateInput = ({ operator }) => {
   const [restakeAmount, setRestakeAmount] = useState(0);
 
   const dylanContractAddress = "0x974C49387efdBB501606b4CC03eE8d19E667175d";
   const vikramContractAddress = "0xE1D008D0E5d431aDa64941D5c9c9A6d1CF6B76C7";
   const keshavContractAddress = "0xFdEdcae5E3Ac257E2Cd6B8a5f6469e7132d3116B";
 
-
   const handleRestake = async () => {
-
-    console.log(operator, restakeAmount)
-    return
+    console.log(operator, restakeAmount);
+    return;
 
     if (!contract || !restakeAmount) {
       alert("Please enter an amount and connect wallet");
@@ -19,9 +17,11 @@ const DelegateInput = ({operator}) => {
     }
 
     try {
-      console.log("Attempt")
-      const tx = await contract.deposit({ value: ethers.parseEther(restakeAmount) });
-      console.log("Transaction!")
+      console.log("Attempt");
+      const tx = await contract.deposit({
+        value: ethers.parseEther(restakeAmount),
+      });
+      console.log("Transaction!");
       await tx.wait();
       alert(`Deposited ${restakeAmount} ETH successfully!`);
     } catch (error) {
@@ -29,37 +29,32 @@ const DelegateInput = ({operator}) => {
       alert("Transaction failed! Check console for details.");
     }
 
-
-
     mongoData = {
       stakerAddress: account,
       operatorName: operator,
       operatorAddress: operator,
       amountStaked: restakeAmount,
-      yieldValue: (Math.random() * (8 - 3) + 3).toFixed(2)
-    }
+      yieldValue: (Math.random() * (8 - 3) + 3).toFixed(2),
+    };
 
-    var mongoResponse = await fetch('http://localhost:3536/add_stake_data', {
-      method: 'POST',
+    var mongoResponse = await fetch("http://localhost:3536/add_stake_data", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ model: mongoModel, benchmark: jsonInputData.benchmark, accuracy: jsonInputData.accuracy, confusion_matrix: jsonInputData.confusion_matrix, transactionHash: transactionHash }),
-    }); 
+      body: JSON.stringify({
+        model: mongoModel,
+        benchmark: jsonInputData.benchmark,
+        accuracy: jsonInputData.accuracy,
+        confusion_matrix: jsonInputData.confusion_matrix,
+        transactionHash: transactionHash,
+      }),
+    });
 
     var mongoData = await mongoResponse.json();
 
-    alert(`Result: ${JSON.stringify(mongoResult)}`)
-
-
-
-      
-
+    alert(`Result: ${JSON.stringify(mongoResult)}`);
   };
-
-
-
-
 
   return (
     <div className="delegate_options">
@@ -77,10 +72,7 @@ const DelegateInput = ({operator}) => {
       />
 
       {/* Button */}
-      <button
-        onClick={handleRestake}
-        className="delegate_button"
-      >
+      <button onClick={handleRestake} className="delegate_button">
         Delegate
       </button>
     </div>
